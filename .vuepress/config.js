151adb98f0ff26f6a46bb8a3fad6981d47087575
Matteo Joliveau <matteo.joliveau@mikamai.com>
2019-12-03 10:26:07 +0100

    Init project with Vuepress

---
+++ .vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: "Enseada",
+  description: "A Cloud native multi-package registry",
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Website', link: 'https://enseada.io', target: '_blank'},
+      { text: 'Source code', link: 'https://github.com/enseadaio/enseada', target: '_blank'},
+    ],
+    sidebar: [
+      '/getting-started',
+      '/install',
+    ]
+  }
+}
