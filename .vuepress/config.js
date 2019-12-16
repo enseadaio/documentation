2c5a07f5ed7b35012ae92949bdee18555b7e7f8d
Matteo Joliveau <matteo.joliveau@mikamai.com>
2019-12-16 15:36:09 +0100

    Updated and refactored

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,14 +2,57 @@ module.exports = {
   title: "Enseada",
   description: "A Cloud native multi-package registry",
   themeConfig: {
+    logo: '/enseada-logo.svg',
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Website', link: 'https://enseada.io', target: '_blank'},
-      { text: 'Source code', link: 'https://github.com/enseadaio/enseada', target: '_blank'},
+      { text: 'Users Guide', link: '/users/' },
+      { text: 'Developers Guide', link: '/developers/' },
+      { text: 'Website', link: 'https://enseada.io', target: '_blank' },
+      { text: 'Source code', link: 'https://github.com/enseadaio/enseada', target: '_blank' },
     ],
-    sidebar: [
-      '/getting-started',
-      '/install',
-    ]
+    displayAllHeaders: true,
+    sidebar: {
+      '/users/': [
+        '',
+        'install',
+        'authentication',
+        'authorization',
+        'package-registries',
+        'ui',
+      ],
+      '/developers/': [
+        'apis',
+      ],
+      '/': [
+        '',
+      ]
+    },
+    // sidebar: [
+    //   {
+    //     title: 'Getting Started',
+    //     path: '/getting-started',
+    //   },
+    //   {
+    //     title: 'Install and Update',
+    //     path: '/install'
+    //   },
+    //   {
+    //     title: 'User Guide',
+    //     path: '/users/',
+    //     children: [
+    //       // 'authentication',
+    //       // 'authorization',
+    //       // 'package-registries',
+    //       // 'ui',
+    //     ]
+    //   },
+    //   {
+    //     title: 'Developer Guide',
+    //     path: '/developers/',
+        
+    //     children: [
+    //       'apis',
+    //     ]
+    //   },
+    // ]
   }
 }
--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 home: true
 heroImage: /logo-white.png
 actionText: Get Started
-actionLink: /getting-started/
+actionLink: /users/
 features:
 - title: Multiple registries
   details: Enseada is built to host multiple package formats, like Maven and NPM packages, Docker images and Ruby gems.
 - title: Built for the Cloud
   details: |
     Enseada is built on distributed technologies to achieve high availability and easy horizontal scaling.
-    Built on top of the Erlang runtime and CouchDB, Enseada is natively distributed and fault-tolerant, reaching high degrees of reliability and security. The storage engine is compatible with many object storage providers to efficiently store your packages in the Cloud.
+    Written in Go and backed by a CouchDB database, Enseada is natively distributed and fault-tolerant, reaching high degrees of reliability and security. The storage engine is compatible with many object storage providers to efficiently store your packages in the Cloud.
 - title: Container native
   details: Enseada is designed to be run in containers, and plays nicely with orchestration tools such as Kubernetes and Swarm.
 footer: MPL 2.0 Licensed | Copyright © 2019-present Matteo Joliveau
